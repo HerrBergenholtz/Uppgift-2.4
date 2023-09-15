258245b10a61b082f45425443e037b511f2b67c3
HerrBergenholtz <zbdv8800@gmail.com>
2023-09-15 13:07:57 +0200

    Add project files.

---
+++ Uppgift 2.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uppgift_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur mycket tjänar person 1?");
+            float person1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hur mycket tjänar person 2");
+            float person2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hur mycket tjänar person3");
+            float person3 = float.Parse(Console.ReadLine());
+
+            float medellön = (person1 + person2 + person3) / 3;
+            Console.WriteLine("Medellönen på företaget är " + medellön);
+
+            Console.ReadKey();
+        }
+    }
+}
